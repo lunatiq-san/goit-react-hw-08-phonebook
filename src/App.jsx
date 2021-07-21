58614ae514d53fff329fcac6f34bdef713bfe122
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-07-21 10:11:07 +0300

    Task 1: Phonebook finished

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,21 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
   };
