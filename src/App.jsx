c53c5ab3e66c5ac6307621fc899124557d5fa32b
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-08-02 16:53:09 +0300

    Feature: ContactForm on hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,84 @@
-import { Component } from 'react';
+import { useState, Component } from 'react';
 import { v4 } from 'uuid';
 
 import Container from './components/Container';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
+
+// function App() {
+//   const [contacts, setContacts] = useState([]);
+//   const [filter, setFilter] = useState('');
+
+//   // При загрузке данных, берем с localStorage контакты
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     const parsedContacts = JSON.parse(contacts);
+// // Если не пустые, сохраняем в стейт
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   changeFilter = event => {
+//     this.setState({ filter: event.currentTarget.value });
+//   };
+
+//   deleteContact = contactId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//     }));
+//   };
+
+//   formSubmitHandler = ({ name, number }) => {
+//     const contact = {
+//       id: v4(),
+//       name,
+//       number,
+//     };
+
+//     const { contacts } = this.state;
+
+//     if (
+//       contacts.find(
+//         ({ name }) => name.toLowerCase() === contact.name.toLowerCase(),
+//       )
+//     ) {
+//       alert(`${name} is already in contacts`);
+//     } else {
+//       this.setState(({ contacts }) => ({
+//         contacts: [contact, ...contacts],
+//       }));
+//     }
+//   };
+
+//     // const { formSubmitHandler, changeFilter, deleteContact } = this;
+//     // const { contacts, filter } = this.state;
+//     // const normalizedFilter = filter.toLowerCase();
+
+//     // const visibleContacts = contacts.filter(contact =>
+//     //   contact.name.toLowerCase().includes(normalizedFilter),
+//     // );
+
+//     return (
+//       <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={formSubmitHandler} />
+//         <h2>Contacts</h2>
+//         <Filter value={filter} onChange={changeFilter} />
+//         <ContactList
+//           contacts={visibleContacts}
+//           onDeleteContact={deleteContact}
+//         />
+//       </Container>
+//     );
+//   };
 
 class App extends Component {
   state = {
@@ -37,7 +111,7 @@ class App extends Component {
     }));
   };
 
-  formSubmitHandler = ({ name, number }) => {
+  formSubmitHandler = (name, number) => {
     const contact = {
       id: v4(),
       name,
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,67 +1,71 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import styles from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = event => {
+  const handleChange = event => {
     const { name, value } = event.currentTarget;
 
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit(name, number);
+    reset();
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label className={styles.label}>
+        Name
+        <input
+          className={styles.input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <label className={styles.label}>
+        Number
+        <input
+          className={styles.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </label>
 
-    return (
-      <form onSubmit={this.handleSubmit} className={styles.form}>
-        <label className={styles.label}>
-          Name
-          <input
-            className={styles.input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            value={name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label className={styles.label}>
-          Number
-          <input
-            className={styles.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-            value={number}
-            onChange={this.handleChange}
-          />
-        </label>
-
-        <button className={styles.btn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+      <button className={styles.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
 
 export default ContactForm;
