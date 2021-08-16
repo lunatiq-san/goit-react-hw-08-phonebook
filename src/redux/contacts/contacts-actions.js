39c84a7e7c4e350dbd52608e1c6b29beca6daa42
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-08-16 15:24:37 +0300

    Feature: createAction()

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,24 +1,39 @@
 import types from './contacts-types';
+import { createAction } from '@reduxjs/toolkit';
 import { v4 } from 'uuid';
 
-const addContact = ({ name, number }) => ({
-  type: types.ADD,
+// v2
+const addContact = createAction(types.ADD, ({ name, number }) => ({
   payload: {
     id: v4(),
     name,
     number,
   },
-});
+}));
+const deleteContact = createAction(types.DELETE);
+const changeFilter = createAction(types.CHANGE_FILTER);
 
-const deleteContact = contactId => ({
-  type: types.DELETE,
-  payload: contactId,
-});
+// v1
+// const addContact = ({ name, number }) => ({
+//   type: types.ADD,
+//   payload: {
+//     id: v4(),
+//     name,
+//     number,
+//   },
+// });
 
-const changeFilter = value => ({
-  type: types.CHANGE_FILTER,
-  payload: value,
-});
+// v1
+// const deleteContact = contactId => ({
+//   type: types.DELETE,
+//   payload: contactId,
+// });
+
+// v1
+// const changeFilter = value => ({
+//   type: types.CHANGE_FILTER,
+//   payload: value,
+// });
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { addContact, deleteContact, changeFilter };
