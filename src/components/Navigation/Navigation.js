0c1b2ad5499510fbbf7245de4813c3ce09ec1dce
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-09-07 14:46:21 +0300

    Hide contacts when user log out

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,25 +1,33 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
 import styles from './Navigation.module.css';
 
-const Navigation = () => (
-  <nav className={styles.nav}>
-    <NavLink
-      exact
-      to="/"
-      className={styles.link}
-      activeClassName={styles.activeLink}
-    >
-      Главная
-    </NavLink>
+const Navigation = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
-    <NavLink
-      to="/contacts"
-      className={styles.link}
-      activeClassName={styles.activeLink}
-    >
-      Контакты
-    </NavLink>
-  </nav>
-);
+  return (
+    <nav className={styles.nav}>
+      <NavLink
+        exact
+        to="/"
+        className={styles.link}
+        activeClassName={styles.activeLink}
+      >
+        Home
+      </NavLink>
+
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          className={styles.link}
+          activeClassName={styles.activeLink}
+        >
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
